9ed27096e3813fd2b72ae6a65b23689ee56939ea
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-07-27 12:14:45 +0530

    switched to mongodb from prisma

--- src/app/apiUnsafe/route.js
+++ src/app/apiUnsafe/route.js
@@ -1,5 +1,6 @@
 import { prisma } from "../db";
 import { redirect } from "next/navigation";
+import clientPromise from "../mongo_db";
 import { NextResponse } from "next/server";
 // import { type NextRequest } from "next/server";
 // import { useRouter } from "next/router";
@@ -22,12 +23,18 @@ export async function POST(req) {
     // const test2 = await test.json()
     // console.log(test2)
     // fetching data from prisma
-    const test = await prisma.users.findMany({
-        where: {
-            username: username,
-            password: password,
-        },
-    });
+    // const test = await prisma.users.findMany({
+    //     where: {
+    //         username: username,
+    //         password: password,
+    //     },
+    // });
+    const client = await clientPromise;
+    const db = client.db("nosqli");
+    const test = await db.collection("users").find({
+        username: username,
+        password: password,
+    }).toArray();
     const test2 = JSON.stringify(test)
     console.log(test2)
     // redirects only occur at server side
