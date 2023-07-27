fd919610a4e92862cc2b162cc2e05f960b48d4ea
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-07-27 11:43:56 +0530

    updated safe

--- src/app/apiSafe/route.ts
+++ src/app/apiSafe/route.ts
@@ -10,8 +10,8 @@ const URL = process.env.NEXT_PUBLIC_VERCEL_URL
 
 export async function POST(req: Request) {
     const data = await req.formData();
-    const username = data.get("username");
-    const password = data.get("password");
+    const username = data.get("username")?.toString();
+    const password = data.get("password")?.toString();
     const email = data.get("email");
     const payload = JSON.stringify({ username: username, password: password});
     const body = JSON.stringify({ payload: payload});
@@ -23,8 +23,21 @@ export async function POST(req: Request) {
         body: body
     });
     const res = await res_data.json()
+    if (res.label === "Malicious") {
+        console.log("Malicious request detected");
+    } else {
+        console.log("Safe request");
+        const user = await prisma.users.findMany({
+            where: {
+                username: username,
+                password: password
+            }
+        })
+        console.log(user);
+        res.user = user;
+    }
     // redirects only occur at server side
     // redirect('/')
-    return NextResponse.json({ payload: payload, res: res });
+    return NextResponse.json({ payload: payload, res: res, });
 }
 
